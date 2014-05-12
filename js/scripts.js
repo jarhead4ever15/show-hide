a1731205f26ed8495586085b02ed886dca25b908
Jonathan Hooley <jjhooley@engr-134-02.eastern.ewu.edu>
2014-05-12 14:17:53 -0700

    version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,16 @@
 // My Scripts
 $(document).foundation();
+
+//Click Link Hide Article
+$('a').click(function(){ 
+
+		$('article').readmore({
+			speed: 1000,
+			maxHeight: 0,
+			startOpen: false
+
+		});
+
+});
+
+
